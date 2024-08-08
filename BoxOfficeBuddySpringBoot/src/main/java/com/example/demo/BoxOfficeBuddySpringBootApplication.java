a69113ecd47902d21f46458b448d0debec189d47
suraj Mundada <158062939+suraj-mundada@users.noreply.github.com>
2024-08-08 10:15:55 +0530

    created frontend and spring boot backend for project

---
+++ BoxOfficeBuddySpringBoot/src/main/java/com/example/demo/BoxOfficeBuddySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxOfficeBuddySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxOfficeBuddySpringBootApplication.class, args);
+	}
+
+}
